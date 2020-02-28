c4634c3276fae27fa696d23dbc0fa4df0df8b0eb
Ranajit <>
2020-02-28 13:38:07 +0530

    initial commit

---
+++ MYPODSWIFT/MYPODSWIFT.h
@@ -0,0 +1,19 @@
+//
+//  MYPODSWIFT.h
+//  MYPODSWIFT
+//
+//  Created by Ranajit Chandra on 28/02/20.
+//  Copyright © 2020 Ranajit Chandra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MYPODSWIFT.
+FOUNDATION_EXPORT double MYPODSWIFTVersionNumber;
+
+//! Project version string for MYPODSWIFT.
+FOUNDATION_EXPORT const unsigned char MYPODSWIFTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MYPODSWIFT/PublicHeader.h>
+
+
---
+++ MypodExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MypodExamples
+//
+//  Created by Ranajit Chandra on 28/02/20.
+//  Copyright © 2020 Ranajit Chandra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
